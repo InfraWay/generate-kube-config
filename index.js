c376496cac2248e2f4a0988925ec9ee60ffa0983
mihma <mihmapro@gmail.com>
2021-09-07 17:08:36 +0100

    using precreated kubeconfig json

--- index.js
+++ index.js
@@ -10,52 +10,11 @@ async function run() {
     await io.mkdirP(workdir);
 
     // Grab user input.
-    const caData = core.getInput("caData");
-    const host = core.getInput("host");
-    const clusterName = core.getInput("clusterName");
-    const namespace = core.getInput("namespace");
-    const serviceAccountName = core.getInput("serviceAccountName");
-    const serviceAccountToken = core.getInput("serviceAccountToken");
-
-    // Construct a kubeconfig object.
-    const serviceAccountFullName = `system:serviceaccount:${namespace}:${serviceAccountName}`
-    const kubeconfig = {
-      apiVersion: "v1",
-      clusters: [
-        {
-          cluster: {
-            "certificate-authority-data": caData,
-            server: host,
-          },
-          name: clusterName
-        }
-      ],
-      contexts: [
-        {
-          context: {
-            cluster: clusterName,
-            namespace: namespace,
-            user: serviceAccountFullName
-          },
-          name: clusterName
-        }
-      ],
-      "current-context": clusterName,
-      kind: "Config",
-      preferences: {},
-      users: [
-        {
-          name: serviceAccountFullName,
-          user: {
-            token: serviceAccountToken
-          }
-        }
-      ]
-    };
+    const kubeconfig = core.getInput("kubeconfig");
 
     // Save the kubeconfig object.
-    const formattedConfig = JSON.stringify(kubeconfig, null, 4);
-    fs.writeFileSync(`${workdir}/config`, formattedConfig);
+    // const formattedConfig = JSON.stringify(kubeconfig, null, 4);
+    fs.writeFileSync(`${workdir}/config`, kubeconfig);
 
     // Set KUBECONFIG environment variable.
     core.exportVariable("KUBECONFIG", `${workdir}/config`);
